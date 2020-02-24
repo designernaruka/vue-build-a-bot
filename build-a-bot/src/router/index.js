8fa7caa6f33ed1529f18b88f0df23127b964e4f9
Designer Naruka <designernaruka@gmail.com>
2020-02-24 23:05:59 +0530

    fixes router

---
+++ build-a-bot/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import router from 'vue-router';
+
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+// import PartInfo from '../parts/PartInfo.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+  }, {
+    path: '/build',
+    name: 'Build',
+    component: RobotBuilder,
+  }],
+});
