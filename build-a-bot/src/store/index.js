67f464ba25a4c721adb3d95cf5b035ba029538cb
Designer Naruka <designernaruka@gmail.com>
2020-02-27 15:30:39 +0530

    cart new pages added

---
+++ build-a-bot/src/store/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    cart: [],
+  },
+  mutations: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot);
+    },
+  },
+});
