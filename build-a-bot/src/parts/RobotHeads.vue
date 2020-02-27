56a14a32f6d28bbe4f7f354ea296a0d6b98fa985
Designer Naruka <designernaruka@gmail.com>
2020-02-27 13:13:33 +0530

    Added Side bar with Nested loops

---
+++ build-a-bot/src/parts/RobotHeads.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Arms</h2>
+    The arms are how your robot will interact with the world.
+    They come in a variety of shapes and functions.
+    <div v-for="(arm, idx) in arms" :key="idx">
+      <h4>{{head.title}}</h4>
+      <div>{{head.description}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'RobotHeads',
+  data() {
+    return { heads: parts.heads };
+  },
+};
+</script>
