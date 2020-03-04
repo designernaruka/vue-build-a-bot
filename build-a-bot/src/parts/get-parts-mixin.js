e829f7760cdc51df74577aa72967de62311428e5
Designer Naruka <designernaruka@gmail.com>
2020-03-04 14:57:37 +0530

    Organizing the Store with Modules Namespacing Modules Accessing Namespaced Getters

--- build-a-bot/src/parts/get-parts-mixin.js
+++ build-a-bot/src/parts/get-parts-mixin.js
@@ -1,10 +1,11 @@
 export default {
   created() {
+    // eslint-disable-next-line no-undef
     this.$store.dispatch(getParts);
   },
   computed: {
     parts() {
-      return this.$store.state.parts || {
+      return this.$store.state.robots.parts || {
         heads: [],
         arms: [],
         torsos: [],
