25d3ace45521c8d62ebfa75b7096b5edd59978b0
Designer Naruka <designernaruka@gmail.com>
2020-03-12 13:32:53 +0530

    code for make lifecycle hooks

--- build-a-bot/src/shared/pin-directive.js
+++ build-a-bot/src/shared/pin-directive.js
@@ -1,7 +1,45 @@
-export default {
-  bind: (elemet) => {
-    elemet.style.position = 'absolute';
-    elemet.style.bottom = '5px';
-    elemet.style.right = '5px';
-  },
-};
+export default function (elemet, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    elemet.style[position] = binding.value[position];
+  });
+  elemet.style.position = 'absolute';
+}
+
+// Backup Code Using Directive Lifecycle Hooks code refactored Defined in top
+// function applyStyle(elemet, binding) {
+//   Object.keys(binding.value).forEach((position) => {
+//     elemet.style[position] = binding.value[position];
+//   });
+//   elemet.style.position = 'absolute';
+// }
+// export default {
+//   bind: (elemet, binding) => {
+//     applyStyle(elemet, binding);
+//   },
+//   update: (elemet, binding) => {
+//     applyStyle(elemet, binding);
+//   },
+//   inserted: (elemet, binding),
+// };
+
+
+// Backup Code Using Directive Lifecycle Hooks
+// export default {
+//   bind: (elemet, binding) => {
+//     //   for option one
+//     // if (binding.arg !== 'position') return;
+
+//     // Object.keys(binding.modifiers).forEach((key) => {
+//     //   elemet.style[key] = '5px';
+//     // });
+//     // for option two
+
+//     Object.keys(binding.value).forEach((position) => {
+//       elemet.style[position] = 'binding.value[position]';
+//     });
+//     elemet.style.position = 'absolute';
+//   },
+//   update: (elemet, binding) => {
+
+//   }
+// };
